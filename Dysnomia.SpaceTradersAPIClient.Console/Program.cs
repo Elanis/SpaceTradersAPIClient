35e4b4860a18a4dfbc91e7951a32845a52e74557
Elanis <elanis@hotmail.com>
2024-02-25 21:59:53 +0100

    Initialize project + Setup API Client

---
+++ Dysnomia.SpaceTradersAPIClient.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Dysnomia.SpaceTradersAPIClient.ConsoleApp {
+    internal class Program {
+        static async Task Main(string[] args) {
+            var httpClient = new HttpClient();
+            var client = new SpaceTradersClient(httpClient);
+
+            var factions = await client.GetFactionsAsync(1, 20);
+            Console.WriteLine("Done");
+        }
+    }
+}
